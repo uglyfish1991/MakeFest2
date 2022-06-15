830ea0ec81175503d976e7fb28602c4b4b09c11b
Katy Long <katylong@michaelplewatch.home>
2022-06-15 11:54:53 +0100

    email form complete, fonts added

--- emailcollection/app.py
+++ emailcollection/app.py
@@ -6,12 +6,12 @@ app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db=SQLAlchemy(app)
 
-class Asset(db.Model):  #sets the class so we can refer to things as class.thingy
+class Person(db.Model):  #sets the class so we can refer to things as class.thingy
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200),nullable=False)
-    genre = db.Column(db.String (150),nullable=False)
-    desc = db.Column(db.String (1000), nullable=False)
-    stock = db.Column(db.Integer, nullable = False)
+    name = db.Column(db.String(200),nullable=False)
+    email = db.Column(db.String (150),nullable=False)
+    number = db.Column(db.String (1000), nullable=False)
+    course = db.Column(db.Integer, nullable = False)
     market = db.Column(db.String (100), nullable=True)
     date_created = db.Column(db.DateTime,default=datetime.utcnow)
     def __repr__(self):
@@ -20,13 +20,13 @@ class Asset(db.Model):  #sets the class so we can refer to things as class.thing
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method=='POST':
-        asset_title = request.form['title']
-        asset_genre = request.form['genre']
-        asset_desc = request.form['desc']
-        asset_stock = request.form['stock']
+        person_name = request.form['name']
+        person_email = request.form['email']
+        person_number = request.form['number']
+        person_course = request.form['course']
         marketing_allowed=request.form.get('market')
 
-        new_item= Asset(title=asset_title,genre=asset_genre,desc=asset_desc,stock=asset_stock, market=marketing_allowed)
+        new_item= Person(name=person_name,email=person_email,number=person_number,course=person_course, market=marketing_allowed)
 
         try:
             db.session.add(new_item)
@@ -35,13 +35,13 @@ def index():
         except:
             return 'There was an issue with adding the item'
     else:
-        asset = Asset.query.all()
-        return render_template('index.html',asset=asset)
+        person = Person.query.all()
+        return render_template('index.html',person=person)
 
 @app.route('/view')
 def viewing():
-    asset = Asset.query.all()
-    return render_template('view.html',asset=asset)
+    person = Person.query.all()
+    return render_template('view.html',person=person)
 
 
 if __name__=="__main__":
